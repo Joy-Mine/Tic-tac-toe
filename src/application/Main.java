c033cbb1d092076518a15e96483b9f3fb5cea4d1
Joy-Mine <1525323329@qq.com>
2022-11-20 19:56:41 +0800

    A trial.

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,6 +2,7 @@ package application;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -14,6 +15,7 @@ public class Main extends Application {
 
             fxmlLoader.setLocation(getClass().getClassLoader().getResource("mainUI.fxml"));
             Pane root = fxmlLoader.load();
+//            Parent root = fxmlLoader.load();
             primaryStage.setTitle("Tic Tac Toe");
             primaryStage.setScene(new Scene(root));
             primaryStage.setResizable(false);
